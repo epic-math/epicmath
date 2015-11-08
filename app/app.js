6ecd60eeb5d2112bf3f8459a8020d9967f577ab0
bogotobogo <k@bogotobogo.com>
2015-11-07 17:36:56 -0800

    initial commit

---
+++ app/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+  'ngRoute',
+  'myApp.home',
+  'myApp.view1',
+  'myApp.view2',
+  'myApp.version'
+]).
+config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/home'});
+}]);
---
+++ app/view1/view1.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+
+.controller('View1Ctrl', [ '$scope', function($scope) {
+$scope.name = '';
+$scope.names = [{name:"Epic"}, {name:"Math"}];
+$scope.addName = function() {
+$scope.names.push( {'name':$scope.name} );
+$scope.name = '';
+};
+}]);
