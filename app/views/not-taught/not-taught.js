14a70041b5d3d0920e557e06241745be2af1cd1e
bogotobogo <k@bogotobogo.com>
2016-02-07 22:33:16 -0800

    added imaginary page

--- app/views/not-taught/not-taught.js
+++ app/views/not-taught/not-taught.js
@@ -6,16 +6,31 @@ angular.module('myApp.views.not-taught', ['ngRoute'])
   $routeProvider
     .when('/not-taught', {
       templateUrl: 'views/not-taught/not-taught.html',
-      controller: 'Not-taughtCtrl'
-  });
+      controller: 'not-taughtCtrl'
+    })
+
+    .when('/not-taught/imaginaryNumber', {
+      templateUrl: 'views/not-taught/imaginaryNumber/imaginaryNumber.html',
+      controller: 'not-taughtCtrl'
+    })
+
+    .when('/not-taught/imaginaryNumber/history', {
+      templateUrl: 'views/not-taught/imaginaryNumber/history.html',
+      controller: 'not-taughtCtrl'
+    })
+
+    .otherwise({redirectTo: '/home'});
 }])
 
-
-.controller('Not-taughtCtrl', [ '$scope', function($scope) {
+.controller('not-taughtCtrl', [ '$scope', function($scope) {
+  /*
   $scope.name = '';
   $scope.names = [{name:"Epic"}, {name:"Math"}];
   $scope.addName = function() {
     $scope.names.push( {'name':$scope.name} );
     $scope.name = '';
   };
+  */
 }]);
+
+
