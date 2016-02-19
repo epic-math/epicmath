95c1ad3afb0c7b7c0bffb377d0ac8a7b42c35e23
bogotobogo <k@bogotobogo.com>
2016-02-18 17:29:31 -0800

    added null space

--- app/views/cmath/linear-algebra/linear-algebra.js
+++ app/views/cmath/linear-algebra/linear-algebra.js
@@ -1,34 +1,25 @@
 'use strict';
 
-angular.module('myApp.views.cmath', ['ngRoute'])
+angular.module('myApp.views.cmath.linear-algebra', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider
-  
-    .when('/cmath', {
-      templateUrl: 'views/cmath/cmath.html',
-      controller: 'cmathCtrl'
-    })
-
+    /*
     .when('/cmath/linear-algebra', {
       templateUrl: 'views/cmath/linear-algebra/linear-algebra.html',
       controller: 'linear-algebraCtrl'
     })
+*/
 
     .when('/cmath/linear-algebra/null-space', {
       templateUrl: 'views/cmath/linear-algebra/null-space/null-space.html',
       controller: 'linear-algebraCtrl'
     })
 
-    .when('/cmath/complex-variables', {
-      templateUrl: 'views/cmath/complex-variables/complex-variables.html',
-      controller: 'cmathCtrl'
-    })
+    .otherwise({redirectTo: '/cmath'});
 
-    .otherwise({redirectTo: '/home'});
 }])
 
-.controller('cmathCtrl', [ '$scope', function($scope) {
+.controller('linear-algebraCtrl', [ '$scope', function($scope) {
 
 }]);
-
