0da2d4add6215a49e3d1d1f6abaa482f3f98fcb4
Ubuntu <ubuntu@ip-172-31-11-56.us-west-1.compute.internal>
2017-12-01 01:49:26 +0000

    Updating sidestory

--- app/views/sidestory/sidestory.js
+++ app/views/sidestory/sidestory.js
@@ -5,17 +5,31 @@ angular.module('myApp.views.sidestory', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider
     .when('/sidestory', {
-      templateUrl: '/views/sidestory/sidestory.html',
-      controller: 'SidestoryCtrl'
-  });
+      templateUrl: 'views/sidestory/sidestory.html',
+      controller: 'sidestoryCtrl'
+    })
+
+    .when('/sidestory/numberStory', {
+      templateUrl: 'views/sidestory/numberStory.html',
+      controller: 'sidestoryCtrl'
+    })
+
+    .when('/sidestory/numberStory/scale', {
+      templateUrl: 'views/sidestory/numberStory/scale.html',
+      controller: 'sidestoryCtrl'
+    })
+
+    .otherwise({redirectTo: '/home'});
 }])
 
 
-.controller('SidestoryCtrl', [ '$scope', function($scope) {
+.controller('sidestoryCtrl', [ '$scope', function($scope) {
+  /*
   $scope.name = '';
   $scope.names = [{name:"Epic"}, {name:"Math"}];
   $scope.addName = function() {
     $scope.names.push( {'name':$scope.name} );
     $scope.name = '';
   };
+  */
 }]);
